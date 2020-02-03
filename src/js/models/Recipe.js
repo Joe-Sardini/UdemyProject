ff9b06b0eec830eca3c899512a67630954bc642e
Nova5555 <Nova5555@gmail.com>
2020-02-02 22:07:30 -0500

    recipe Updates

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -17,4 +17,15 @@ export default class Recipe {
             console.log(error);
         }
     }
+
+    calcTime() {
+        //15 mins for each 3 ingredients ??
+        const numIng = this.ingredients.length;
+        const periods = Math.ceil(numIng/3);
+        this.time = periods * 15;
+    }
+
+    calcServings() {
+        this.servings = 4;
+    }
 }
